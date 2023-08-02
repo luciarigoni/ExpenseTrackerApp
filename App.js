4b724655f4da2b8935613cbba52e3da2d9630415
Lucia Rigoni Freire <luciarigoni@live.com>
2023-08-02 11:06:12 -0300

    Adding global colors and editing navigation tabs

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { Ionicons } from '@expo/vector-icons'
 
 import ManageExpense from './screens/ManageExpense'
 import RecentExpenses from './screens/RecentExpenses'
@@ -19,9 +20,18 @@ function ExpensesOverview() {
       headerTintColor: 'white',
       tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
       tabBarActiveTintColor: GlobalStyles.colors.accent500
-    }}>
-    <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses}/>
-    <BottomTabs.Screen name='AllExpenses' component={AllExpenses}/>
+    }}
+    >
+    <BottomTabs.Screen name='RecentExpenses' component={RecentExpenses} options={{
+      title: 'Recent Expenses',
+      tabBarLabel: 'Recent',
+      tabBarIcon: ({color, size}) => <Ionicons name='hourglass' size={size} color={color}/>  
+    }}/>
+    <BottomTabs.Screen name='AllExpenses' component={AllExpenses} options={{
+      title: 'All Expenses',
+      tabBarLabel: 'All Expenses',
+      tabBarIcon: ({color, size}) => <Ionicons name='calendar' size={size} color={color}/>  
+    }}/>
   </BottomTabs.Navigator>
   );
 }
