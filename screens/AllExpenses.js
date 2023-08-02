8997a936dd61f88a3baada8c67efe85a81c23194
Lucia Rigoni Freire <luciarigoni@live.com>
2023-08-02 09:16:33 -0300

    Create new screens

---
+++ screens/AllExpenses.js
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+function AllExpenses() {
+    <Text>AllExpenses Screen</Text>
+}
+
+export default AllExpenses;
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,6 @@
+import { Text } from "react-native";
+function RecentExpenses() {
+    <Text>RecentExpenses Screen</Text>
+}
+
+export default RecentExpenses;
